b825eba304ce66136b660284bc8581cfce8d2e85
ryosuke <241036@st.yoshida-g.ac.jp>
2024-12-24 11:18:39 +0900

    1224

---
+++ user.php
@@ -0,0 +1,35 @@
+
+   <link rel="stylesheet" href="css.php"> 
+   <?php
+    include "open_db.php";
+    
+    $body =<<<___EOF___
+
+    <header>
+        <h1>cyber</h1>
+    </header>
+    <h2>ログイン</h2>
+    <br>
+    <br>
+    <center>
+    <form method="POST">
+        <p>ニックネーム：<input type="text" name="title" required></p>
+        <p>メールアドレス：<input type="email" name="name" required></p>
+        <p>パスワード：<input type="password" name="password" required></p>
+        <br>
+        <input type="submit" value="ログイン">
+     </form>
+     </center>
+     <footer>
+        
+        <a style="border: 2px solid black;" href="login.php">新規登録はこちらから</a>
+         
+     </footer>  
+    
+    ___EOF___;
+echo  $body;
+?>
+   
+     
+
+
