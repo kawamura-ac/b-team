8dacb54fc5ea7d4830086df0c8ced5c911a9a51d
nei3055 <nei3055@naver.com>
2025-01-17 13:39:25 +0900

    0117 投稿画面のCSS変更と戻るボタン追加修正しました。

--- create_post.php
+++ create_post.php
@@ -9,20 +9,24 @@ if (!isset($_SESSION['user_id'])) {
 
     $alert = ""; //タイトル文字制限
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
     $title = trim($_POST['post_title']);               // 変数名以外のtitleを全てpost_titleに変更
-    $content = trim($_POST['post_content']);           // 変数名以外のcontentを全てpost_contentに変更
+    $content = isset($_POST['post_content']);           // 変数名以外のcontentを全てpost_contentに変更
     $author_id = $_SESSION['user_id'];           // 変数名以外のauthor_idを全てuser_idに変更
 
-    $titleLength = mb_strlen($title, 'UTF-8');  //タイトル文字数の制限 ここから
-    
-    if($titleLength<2){
-        $alert = "タイトルは1文字以上の入力をする必要があります。";
-    }elseif($titleLength>30){
-        $alert = "タイトルは30文字以下の入力をする必要があります。";
-    }else{  //ここまで
+    if(empty($title)){  //タイトル文字数の制限 ここから
+        $alert="タイトルを入力してください。";
+    }else{
+        $titleLength = mb_strlen($title, 'UTF-8');
+        if($titleLength<2){
+            $alert = "タイトルは1文字以上の入力をする必要があります。";
+        }elseif($titleLength>30){
+            $alert = "タイトルは30文字以下の入力をする必要があります。";
+        }else{  //ここまで
 
-    $stmt = $pdo->prepare("INSERT INTO posts (post_title, post_content, user_id) VALUES (:post_title, :post_content, :user_id)");
+    $stmt = $pdo->prepare(
+        "INSERT INTO posts (post_title, post_content, user_id) 
+         VALUES (:post_title, :post_content, :user_id)");
     $stmt->execute([
         'post_title' => $title, 
         'post_content' => $content, 
@@ -43,22 +47,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>投稿を作成</title>
 </head>
 <body>
-    <div class="container">
-        <h2>投稿作成画面</h2>
+    <div class="post_container">
+        <h2>投稿作成</h2>
         <form action="create_post.php" method="POST">
-            <label for="post_title">タイトル</label>
 
-            <!--タイトル文字制限のアラム-->
+        <!--タイトル文字制限のアラム-->
+            <label for="post_title">タイトル</label>
             <?php if(!empty($alert)):?> 
                 <p style="color:red; margin-top:-10px; margin-bottom:10px;">
                     <?php echo htmlspecialchars($alert,ENT_QUOTES,'UTF-8');?>
                 </p>
             <?php endif;?>
-
             <input type="text" name="post_title" id="post_title">
+
+        <!--テキストエリア-->
+            <div class="textarea">
             <label for="post_content">投稿内容</label>
             <textarea name="post_content" id="post_content"></textarea>
-            <button type="submit">投稿</button>
+
+        <!--ボタン-->
+                <button type="submit">投稿する</button>
+        
+        <!--メイン画面に戻る-->
+                <p style="padding-top:-10px;" border: 1px solid #ccc;">
+                <button type="button" onclick="window.location.href='main.php';">メインに戻る</button></p>
         </form>
     </div>
 </body>
