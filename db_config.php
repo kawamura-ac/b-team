9fd2b00df2edc8dea057c2f44e08fa741835678c
nei3055 <nei3055@naver.com>
2025-01-20 10:20:46 +0900

    --

--- db_config.php
+++ db_config.php
@@ -4,7 +4,6 @@ $dbname = "team_b";
 $username = "LAA1618183";
 $password = "dbpasswd";         
 
-
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- index.php
+++ index.php
@@ -5,19 +5,21 @@ require 'db_config.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nickname = $_POST['user_name'];    // 変数名以外のnicknameを全てuser_nameに変更
     $password = $_POST['user_paw'];    // 変数名以外のpasswordを全てuser_pawに変更
-
+    
     $stmt = $pdo->prepare("SELECT * FROM users WHERE user_name = :user_name");
     $stmt->execute(['user_name' => $nickname]);
     $user = $stmt->fetch();
+   
 
     // password_verify — パスワードがハッシュにマッチするかどうかを調べる
-    if ($user && password_verify($password, $user['user_paw'])) {  // データベースの user_pawをvarchar(255)にしないとハッシュ化されたパスワードが入らない
-        $_SESSION['user_id'] = $user['user_id'];
-        $_SESSION['user_name'] = $user['user_name'];
-        header('Location: main.php');
-        exit();
-    } else {
-        $error = "ニックネームまたはパスワードが違います。";
+        if ($user && password_verify($password, $user['user_paw'])) {  // データベースの user_pawをvarchar(255)にしないとハッシュ化されたパスワードが入らない
+            $_SESSION['user_id'] = $user['user_id'];
+            $_SESSION['user_name'] = $user['user_name'];
+            header('Location: main.php');
+            exit();
+        } else {
+            $error = "ニックネームまたはパスワードが間違っています";
+        }
     }
 
 ?>
@@ -37,23 +39,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <?php if (!empty($error)): ?>
                 <p class="error"><?php echo $error; ?></p>
             <?php endif; ?>
-<<<<<<< HEAD
-            <label for="user_name">ニックネーム</label>
-            <input type="text" name="user_name" id="user_name" required>
-            
-=======
             
             <div id="nickname-error" style="color: red; display: none;">ニックネームは20文字以内で入力してください。</div>
-            <div id="email-error" style="color: red; display: none;">メールアドレスは30文字以内で入力してください。</div>
             <div id="password-error" style="color: red; display: none;">パスワードは20文字以内で入力してください。</div>
 
             <label for="user_name">ニックネーム</label>
             <input type="text" name="user_name" id="user_name" required>
 
-            <label for="user_email">メールアドレス</label>
-            <input type="email" name="user_email" id="user_email" required>
-            
->>>>>>> f58c18b8ebd8b68200e9be25b533f7f865a61999
             <label for="user_paw">パスワード</label>
             <input type="password" name="user_paw" id="user_paw" required>
             
@@ -87,11 +79,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             
         } 
 
-        // メールアドレスの文字数制限チェック
-        if (emailInput.length > 30) {
-            emailError.style.display = "block";
-            isValid = false;
-        }
 
         // パスワードの文字数制限チェック
          if (passwdInput.length > 20) {
